d94ebc70ff8b3a426e76cb7ea821d661b1a687e5
Ubuntu <mcarneiro2000@gmail.com>
2026-05-27 09:50:25 -0400

    removed exposed secrets

--- yt-dictionary/src/environments/environment.ts
+++ yt-dictionary/src/environments/environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: false,
-  supabaseUrl: 'https://pbtezriwfqxodizdymhf.supabase.co/rest/v1/',
-  supabaseKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InBidGV6cml3ZnF4b2RpemR5bWhmIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Nzk4Nzc2ODMsImV4cCI6MjA5NTQ1MzY4M30.yrmIjwqiYd-hNl0XmyGE0nZoyOs-scT-sH_mfh3wdno',
+  supabaseUrl: 'YOUR_SUPABASE_URL',
+  supabaseKey: 'YOUR_SUPABASE_ANON_KEY'
 };
